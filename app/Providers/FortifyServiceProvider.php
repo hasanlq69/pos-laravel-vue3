1a43e7e16690fe7b535af71d8553bc9fe6a7e187
hasanlq69 <hasanlq69@gmail.com>
2025-05-09 11:17:23 +0700

    Add overid fortify

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -59,5 +59,10 @@ class FortifyServiceProvider extends ServiceProvider
                 'request' => $request,
             ]);
         });
+
+        /**
+         * logout
+         */
+        $this->app->singleton(\Laravel\Fortify\Contracts\LogoutResponse::class,\App\Http\Responses\LogoutResponse::class);
     }
 }
